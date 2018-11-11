c88a6caf43076025425a3c5cc6459699bcff410a
Christopher Yeung <christopheryeung1@gmail.com>
2018-11-10 20:45:52 -0500

    Add fade-in/out animation, update buttons and settings logic

--- scripts.js
+++ scripts.js
@@ -3,7 +3,11 @@ const session = document.querySelector('#session');
 const time = document.querySelector('#time');
 const work = document.querySelector('#work');
 const rest = document.querySelector('#rest');
+const title = document.querySelector('.title');
+const settings = document.querySelector('.settings');
 let totalSeconds, timer;
+let play = false;
+
 
 function convertToSeconds() {
     let duration = (time.textContent).split(':');
@@ -33,17 +37,33 @@ function startTimer() {
 
 function stopTimer() {
     clearInterval(timer);
+    stylize('#fff');
     time.textContent = work.textContent + ':00';
 }
 
+function resetTimer() {
+    clearInterval(timer);
+    session.textContent = 'Work';
+    work.textContent = '25';
+    rest.textContent = '5';
+    time.textContent = '25:00';
+}
+
 function switchSession() {
-    if (session.textContent == 'Work') {
+    if (session.textContent === 'Work') {
         session.textContent = 'Rest';
+        stylize('rgb(66, 185, 245)');
         totalSeconds = rest.textContent * 60;
     } else {
         session.textContent = 'Work';
+        stylize('#fff');
         totalSeconds = rest.textContent * 60;
     }
+}
+
+function stylize(color) {
+    session.style.color = color;
+    time.style.color = color;
 }
 
 function decrease(activity) {
@@ -64,9 +84,19 @@ function increase(activity, limit) {
 
 function updateTime() {
     let duration = (time.textContent).split(':');
-    let workValue = parseInt(work.textContent)
+    let workValue = parseInt(work.textContent);
     duration[0] = workValue < 10 ? '0' + workValue : workValue;
     time.textContent = duration.join(':');
+}
+
+function hideElements() {
+    title.style.opacity = 0;
+    settings.style.opacity = 0;
+}
+
+function showElements() {
+    title.style.opacity = 1;
+    settings.style.opacity = 1;
 }
 
 buttons.forEach(button => button.addEventListener('click', function(e) {
@@ -74,30 +104,61 @@ buttons.forEach(button => button.addEventListener('click', function(e) {
     tId = e.target.id;
     switch(tId) {
         case 'play':
-            startTimer();
+            if (play === true) {
+                return;
+            } else {
+                play = true;
+                pause = false;
+                startTimer();
+                hideElements();
+            }
             break;
         case 'pause':
             clearInterval(timer);
+            play = false;
+            pause = true;
             break;
         case 'stop':
             stopTimer();
+            showElements();
+            play = false;
+            pause = false;
             break;
         case 'reset':
-            location.reload();
+            resetTimer();
+            showElements();
+            play = false;
+            pause = false;
             break;
         case 'dec-work':
-            decrease(work);
-            updateTime();
+            if (play == true || pause == true) {
+                return;
+            } else {
+                decrease(work);
+                updateTime();
+            }
             break;
         case 'inc-work':
-            increase(work, 60);
-            updateTime();
+            if (play == true || pause == true) {
+                return;
+            } else {
+                increase(work, 60);
+                updateTime();
+            }
             break;
         case 'dec-rest':
-            decrease(rest);
+            if (play == true || pause == true) {
+                return;
+            } else {
+                decrease(rest);
+            }
             break;
         case 'inc-rest':
-            increase(rest, 30);
+            if (play == true || pause == true) {
+                return;
+            } else {
+                increase(rest, 30);
+            }
             break;
     }
 }));
